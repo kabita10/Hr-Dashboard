bfbd5f843945a0f98095ed6a3f7dc1a4590f3f5c
Susmita438 <poudelsusmita438@gmail.com>
2020-11-24 12:28:35 +0545

    Added hr dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HrDashboardComponent } from './hr-dashboard/hr-dashboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+{ path: 'dashboard', component: HrDashboardComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
