20344bea25502dabbeb5c9489b4831d2dc0e2248
Susmita438 <poudelsusmita438@gmail.com>
2020-12-03 13:43:55 +0545

    Ui fixes and sidebar design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,13 +11,9 @@ import { ApplicantsListComponent } from './applicants-list/applicants-list.compo
 import { ApplicantsDetailComponent } from './applicants-detail/applicants-detail.component';
 import { MultiStepFormComponent } from './multi-step-form/multi-step-form.component';
 import { UserJobDetailsComponent } from './user-job-details/user-job-details.component';
-import { NgxPaginationModule } from 'ngx-pagination';
+// import { NgxPaginationModule } from 'ngx-pagination';
 import { Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'hr-dashboard', component: HrDashboardComponent },
-];
 
 @NgModule({
   declarations: [
@@ -33,7 +29,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxPaginationModule
+    // NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
